a7fb06c40f9613f30f2c53a7c51200170c3e8287
jisse Reitsma <jisse@yireo.com>
2022-02-09 09:37:08 +0100

    Adding all files to git via Yireo Command

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Yireo_IntegrationTestHelper',
+    __DIR__
+);
