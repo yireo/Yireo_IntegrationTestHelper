6b4b0dd21fa31583e44f5958d6c2163997bda726
Jisse Reitsma <jisse@yireo.com>
2024-03-11 11:34:28 +0100

    Wrap CURL error

--- Check/SearchEngineCheck.php
+++ Check/SearchEngineCheck.php
@@ -3,13 +3,14 @@
 namespace Yireo\IntegrationTestHelper\Check;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Yireo\IntegrationTestHelper\Utilities\CurrentInstallConfig;
 
 class SearchEngineCheck
 {
     private CurrentInstallConfig $currentInstallConfig;
     private Client $httpClient;
-    
+
     public function __construct(
         CurrentInstallConfig $currentInstallConfig,
         Client $httpClient
@@ -17,11 +18,16 @@ class SearchEngineCheck
         $this->currentInstallConfig = $currentInstallConfig;
         $this->httpClient = $httpClient;
     }
-    
+
     public function checkSearchEngineConnection(): bool
     {
        $config = $this->currentInstallConfig->getValues();
-       $response = $this->httpClient->get($config['elasticsearch-host'].':'.$config['elasticsearch-port']);
-       return $response->getStatusCode() === 200;
+        try {
+            $response = $this->httpClient->get($config['elasticsearch-host'].':'.$config['elasticsearch-port']);
+        } catch (GuzzleException $e) {
+            return false;
+        }
+
+        return $response->getStatusCode() === 200;
     }
 }
